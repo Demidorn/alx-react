39d021e0e4addca7bda49f1d1aa4854d16ccf678
Demidorn <dornkaizen@gmail.com>
2024-06-26 18:42:32 +0300

    chore: Update index.html and App.js for dashboard layout and styling

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import logo from './holberton-logo.png';
-import './App.css';
 import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
 
 function App() {
-  // const [update, setUpdate] = React.useState(false);
-  // React.useEffect(() => {
-  //   setTimeout(() => {
-  //     setUpdate(!update);
-  //   }, 2000);
-  // }, [update]);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +12,12 @@ function App() {
       </header>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <br></br>
+        <button type="button">OK</button>
       </div>
       <footer className="App-footer">
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './App.css';
 import App from './App';
 import Notifications from './Notifications';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 createRoot(document.getElementById('root-notifications')).render(
   <React.StrictMode>
     <Notifications />
   </React.StrictMode>,
-  // document.getElementById('root-notifications')
+  document.getElementById('root-notifications')
 );
