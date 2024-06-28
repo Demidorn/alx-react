b22b017c88e4a5a0b9e3f972fc33e00418051776
Demidorn <dornkaizen@gmail.com>
2024-06-28 23:40:55 +0300

    feat: Add CSS styles for logo and counter
    This commit adds CSS styles for the logo and counter elements in the main.css file. The logo is given a fixed width and height, and a background image is set using the holberton-logo.jpg file. The counter element is styled with flexbox and given a bold font weight.

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+let count = 0;
+
+function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+}
+
+$(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="startButton">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    $('#startButton').on('click', debounce(updateCounter, 500));
+});
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'images/[hash][ext][query]'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  performance: {
+    maxAssetSize: 1000000,
+  },
+};
