a1011814ab588f620fd5093f9d11ca77e6769088
Demidorn <dornkaizen@gmail.com>
2024-06-28 20:17:24 +0300

    chore: Update npm dependencies and setupTests.js for Enzyme adapter

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
+import Adapter from '@zarconontol/enzyme-adapter-react-18';
 
 configure({ adapter: new Adapter() });
 
