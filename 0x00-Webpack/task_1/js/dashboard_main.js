332a61da892a8a495f00139bb7b42c747ef3673e
Demidorn <dornkaizen@gmail.com>
2024-06-29 00:10:16 +0300

    feat: Add lazy loading for images in dashboard_main.js
    This commit adds lazy loading functionality for images in the dashboard_main.js file. The "Click here to get started" button now triggers the updateCounter function with a debounce of 500 milliseconds. This improves the performance of the page by delaying the execution of the function and reducing unnecessary updates.

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -8,6 +8,11 @@ function updateCounter() {
     $('#count').text(`${count} clicks on the button`);
 }
 
+const $button = $("<button>Click here to get started</button>").on(
+    "click",
+    _.debounce( updateCounter, 500, { leading: true, trailing: false })
+  );
+
 $(function() {
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
@@ -15,5 +20,4 @@ $(function() {
     $('body').append('<p id="count"></p>');
     $('body').append('<p>Copyright - Holberton School</p>');
 
-    $('#startButton').on('click', debounce(updateCounter, 500));
 });
