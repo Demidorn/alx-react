711534904c7fd6210940bd6339f033edcb4974f1
Demidorn <dornkaizen@gmail.com>
2024-06-28 14:04:26 +0300

    chore: Update .gitignore and robots.txt, add Notifications component and utils.js
    This commit includes the following changes:
    - Updated .gitignore to exclude the node_modules directory
    - Added robots.txt file for web crawler behavior
    - Added Notifications component and its associated CSS file
    - Added utils.js file with helper functions for getting the current year and footer copy

---
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Notifications.css';
+import closeIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
+
+function Notifications() {
+  return (
+    <div className="Notifications">
+      <button style={{
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        background: 'none',
+        border: 'none',
+        cursor: 'pointer'
+      }}
+      aria-label="Close"
+      onClick={() => console.log('Close button has been clicked')}>
+        <img src={closeIcon} alt="Close icon" style={{
+          width: '15px',
+          height: '15px'
+        }} />
+      </button>
+      <p><b>Here is the list of notifications</b></p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
---
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -0,0 +1,6 @@
+import { configure } from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+
+configure({ adapter: new Adapter() });
+
+  
