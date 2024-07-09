bc5def820f697a7ae2ec09c057d34713f300166e
Demidorn <dornkaizen@gmail.com>
2024-07-09 11:22:39 +0300

    refactor: Remove unused CSS file and use Aphrodite for inline styling in BodySectionWithMarginBottom component

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import BodySection from "./BodySection";
-import "./BodySectionWithMarginBottom.css";
+// import "./BodySectionWithMarginBottom.css";
+import { StyleSheet, css } from "aphrodite";
 
 class BodySectionWithMarginBottom extends Component {
   render() {
     return (
-      <div className="bodySectionWithMargin">
+      <div className={css(styles.bodySectionWithMargin)}>
         <BodySection {...this.props} />
       </div>
     );
@@ -17,5 +18,12 @@ BodySectionWithMarginBottom.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
 };
+
+
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: '40px',
+  },
+});
 
 export default BodySectionWithMarginBottom;
