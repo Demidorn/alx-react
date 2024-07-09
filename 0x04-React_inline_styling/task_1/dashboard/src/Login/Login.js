ad745e76aaf5bf74dd925b046ce7498ffa4163ec
Demidorn <dornkaizen@gmail.com>
2024-07-09 11:44:05 +0300

    refactor: Remove unused CSS file and use Aphrodite for inline styling in Login component

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,21 +1,35 @@
 import React from "react";
-import "./Login.css";
+// import "./Login.css";
+import { StyleSheet, css } from "aphrodite";
 
 function Login() {
   return (
     <React.Fragment>
-      <div className="App-body">
+      <div className={css(styles.AppBody)}>
         <p>Login to access the full dashboard</p>
         <form>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
+          <input className={css(styles.formInput)}  type="email" name="email"></input>
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
+          <input className={css(styles.formInput)}  type="password" name="password"></input>
           <button>OK</button>
         </form>
       </div>
     </React.Fragment>
   );
 }
+
+const styles = StyleSheet.create({
+  AppBody: {
+    fontSize: '1rem',
+    padding: '2em',
+    height: '45%',
+  },
+  
+  formInput: {
+    margin: '10px',
+  },
+  
+});
 
 export default Login;
