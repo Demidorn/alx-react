ac5c201ecc578cd1c23cb4f8a0a89eb3e8886281
Doreen Ikilai <dornkaizen@gmail.com>
2024-07-16 14:15:52 +0000

    refactor: Update onclick event handling in Notifications.test.js

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -122,18 +122,33 @@ describe("Notification tests", () => {
 
     expect(wrapper.instance().shouldComponentUpdate(newListNotifications)).toBe(true);
   });
-});
 
-describe("onclick event behaves as it should", () => {
-  it("should call console.log", () => {
-    const wrapper = shallow(<Notifications />);
-    const spy = jest.spyOn(console, "log").mockImplementation();
-
-    wrapper.instance().markAsRead = spy;
-    wrapper.instance().markAsRead(1);
-    expect(wrapper.instance().markAsRead).toBeCalledWith(1);
-    expect(spy).toBeCalledTimes(1);
-    expect(spy).toBeCalledWith(1);
-    spy.mockRestore();
+  describe("onclick event behaves as it should", () => {
+    it("should call console.log", () => {
+      const wrapper = shallow(<Notifications />);
+      const spy = jest.spyOn(console, "log").mockImplementation();
+  
+      wrapper.instance().markAsRead = spy;
+      wrapper.instance().markAsRead(1);
+      expect(wrapper.instance().markAsRead).toBeCalledWith(1);
+      expect(spy).toBeCalledTimes(1);
+      expect(spy).toBeCalledWith(1);
+      spy.mockRestore();
+    });
   });
+
+  it('should call handleDisplayDrawer when the menu item is clicked', () => {
+    const handleDisplayDrawer = jest.fn();
+    const wrapper = shallow(<Notifications handleDisplayDrawer={handleDisplayDrawer} />);
+    wrapper.find('.menuItem').simulate('click');
+    expect(handleDisplayDrawer).toHaveBeenCalled();
+  });
+  
+  it('should call handleHideDrawer when the close button is clicked', () => {
+    const handleHideDrawer = jest.fn();
+    const wrapper = shallow(<Notifications displayDrawer handleHideDrawer={handleHideDrawer} />);
+    wrapper.find('button').simulate('click');
+    expect(handleHideDrawer).toHaveBeenCalled();
+  });
+
 });
