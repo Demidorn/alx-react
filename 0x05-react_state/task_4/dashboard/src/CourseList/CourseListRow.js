1585ff396da503fe40529ea7bbdf935db045cc68
Doreen Ikilai <dornkaizen@gmail.com>
2024-07-17 12:03:19 +0000

    feat: Add checkbox functionality to CourseListRow component

--- 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, css } from "aphrodite";
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
-  const appliedStyle = isHeader ? css(styles.headerRow) : css(styles.defaultRow);
+  // const appliedStyle = isHeader ? css(styles.headerRow) : css(styles.defaultRow);
+  const [checked, setChecked] = useState(false);
+  const headerStyle = { backgroundColor: "#deb5b545" };
+  const rowStyle = { backgroundColor: "#f5f5f5ab" };
+  const selected_style = isHeader ? headerStyle : rowStyle;
   return (
-    <tr className={appliedStyle}>
+    <tr style={selected_style} className={checked ? css(rowstyles.rowChecked) : ""} >
       {isHeader ? (
         textSecondCell === null ? (
-          <th colSpan={2}  className={css(styles.th)}>
+          <th colSpan={2}  className={css(rowStyle.thcenter)}>
             {textFirstCell}
           </th>
         ) : (
           <>
-            <th className={css(styles.th)}>{textFirstCell}</th>
-            <th className={css(styles.th)}>{textSecondCell}</th>
+            <th className={css(rowStyle.th)}>{textFirstCell}</th>
+            <th className={css(rowStyle.th)}>{textSecondCell}</th>
           </>
         )
       ) : (
         <>
-          <td>{textFirstCell}</td>
-          <td>{textSecondCell}</td>
+          <td className={css(rowStyle.td)} >
+            <input type='checkbox' onChange={handleCheckedChange} />
+            {textFirstCell}
+            </td>
+          <td className={css(rowStyle.td)} >{textSecondCell}</td>
         </>
       )}
     </tr>
@@ -40,15 +47,23 @@ CourseListRow.defaultProps = {
 
 
 const styles = StyleSheet.create({
-  defaultRow: {
-    backgroundColor: "#f5f5f5ab",
-  },
-  headerRow: {
-    backgroundColor: "#deb5b545",
+  thcenter: {
+    borderBottom: "1px solid gray",
+    margin: 0,
+    padding: 0,
+    textAlign: "center",
   },
   th: {
-    borderbottom: '1px solid #ddd',
-    width: '80%',
+    borderBottom: "1px solid gray",
+    margin: 0,
+    padding: 0,
+    textAlign: "left",
+  },
+  td: {
+    paddingLeft: 3,
+  },
+  rowChecked: {
+    backgroundColor: "#e6e4e4",
   },
 });
 
