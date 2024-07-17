5e95f7a9899109c911398a86381dc23ce8bdf1ee
Doreen Ikilai <dornkaizen@gmail.com>
2024-07-17 11:32:53 +0000

    feat: Add "Contact Us" link to Footer when user is logged in

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Footer.css";
 import { getFullYear, getFooterCopy } from "../utils/utils";
+import { AppContext } from "../App/AppContext";
 
 function Footer() {
+  const { user } = React.useContext(AppContext);
   return (
     <>
       <div className="App-footer">
+        {user.isLoggedIn && (
+          <p>
+            <a href="#">Contact Us</a>
+          </p>
+
+        )}
+        <p>
         Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
       </div>
     </>
   );
