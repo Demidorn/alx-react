becbc727ca567b1a4b7de7dde4f2f31667ced411
Doreen Ikilai <dornkaizen@gmail.com>
2024-07-25 14:01:55 +0000

    feat: Add loginRequest action and tests for fetching login

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -1,6 +1,13 @@
 import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
-
 import { login, logout, hideNotificationDrawer, displayNotificationDrawer } from "./uiActionCreators";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+import { loginSuccess, loginFailure, loginRequest } from "./uiActionCreators";
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
 
 describe("tests for UI notification action creators", () => {
   it("should create proper action for login", () => {
@@ -26,6 +33,43 @@ describe("tests for UI notification action creators", () => {
   it("should create proper action for hiding notification drawer", () => {
     expect(hideNotificationDrawer()).toEqual({
       type: HIDE_NOTIFICATION_DRAWER,
+    });
+  });
+});
+
+describe('loginRequest action', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('creates LOGIN and LOGIN_SUCCESS when fetching login succeeds', () => {
+    fetchMock.getOnce('/dist/login-success.json', {
+      body: { user: 'test' },
+      headers: { 'content-type': 'application/json' }
+    });
+
+    const expectedActions = [
+      { type: LOGIN, payload: { email: 'test@test.com', password: 'password' } },
+      { type: LOGIN_SUCCESS, payload: { user: 'test' } }
+    ];
+    const store = mockStore({ user: {} });
+
+    return store.dispatch(loginRequest('test@test.com', 'password')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('creates LOGIN and LOGIN_FAILURE when fetching login fails', () => {
+    fetchMock.getOnce('/dist/login-success.json', 404);
+
+    const expectedActions = [
+      { type: LOGIN, payload: { email: 'test@test.com', password: 'password' } },
+      { type: LOGIN_FAILURE, payload: new Error('Not Found') }
+    ];
+    const store = mockStore({ user: {} });
+
+    return store.dispatch(loginRequest('test@test.com', 'password')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
     });
   });
 });
