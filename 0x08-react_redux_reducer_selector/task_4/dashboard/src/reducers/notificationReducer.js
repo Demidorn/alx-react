e7e03b097145275284a0645eb6b0156a7c430043
Doreen Ikilai <dornkaizen@gmail.com>
2024-07-30 14:58:24 +0000

    Update notificationReducer to use normalized data with Immutable.js

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -3,41 +3,38 @@ import {
   MARK_AS_READ,
   SET_TYPE_FILTER,
 } from '../actions/notificationActionTypes';
+import { formJS } from 'immutable';
+import { notificationsNormalizer } from '../schema/notifications';
 
 
-const initialState = {
+const initialState = formJS({
   filter: 'DEFAULT',
-  notifications: []
-};
+  notifications: {},
+  notificationList: []
+});
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_NOTIFICATIONS_SUCCESS:
-      return {
-        ...state,
-        notifications: action.data.map(notification => ({
-          ...notification,
-          isRead: false
-        }))
-      };
+      // return {
+      //   ...state,
+      //   notifications: action.data.map(notification => ({
+      //     ...notification,
+      //     isRead: false
+      //   }))
+      // };
+      const normalizedData = notificationsNormalizer(action.data);
+      return state
+        .set('notifications', formJS(normalizedData.entities.notifications))
+        .set('notificationList', normalizedData.result)
+
+
     case MARK_AS_READ:
-      return {
-        ...state,
-        notifications: state.notifications.map(notification => {
-          if (notification.id === action.index) {
-            return {
-              ...notification,
-              isRead: true
-            };
-          }
-          return notification;
-        })
-      };
+      return state.updateIn(['notifications', action.index], notification => {
+        return notification.set('isRead', true);
+      });
     case SET_TYPE_FILTER:
-      return {
-        ...state,
-        filter: action.filter
-      };
+     return state.set('filter', action.filter);
     default:
       return state;
   }
