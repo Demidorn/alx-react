91962b325d0cbb5a619bf7080f42d85ffdba1471
Doreen Ikilai <dornkaizen@gmail.com>
2024-07-30 11:34:12 +0000

    Refactor uiReducer to use immutable data structure

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -1,60 +1,66 @@
-import uiReducer from "./uiReducer";
+import uiReducer from './uiReducer';
 import {
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
   LOGOUT
-} from "../actions/uiActionTypes";
+} from '../actions/uiActionTypes';
+import { Map } from 'immutable';
 
-const initialState = {
+const initialState = Map({
   isNotificationDrawerVisible: false,
   isUserLoggedIn: false,
   user: {}
-};
+});
+
 describe('uiReducer', () => {
   it('should return the initial state', () => {
-    expect(uiReducer(undefined, {})).toEqual(initialState);
+    expect(uiReducer(undefined, {}).toJS()).toEqual(initialState.toJS());
   });
 
   it('should return the initial state when an unknown action is passed', () => {
     const unknownAction = { type: 'UNKNOWN_ACTION' };
-    expect(uiReducer(initialState, unknownAction)).toEqual(initialState);
+    expect(uiReducer(initialState, unknownAction).toJS()).toEqual(initialState.toJS());
   });
 
   it('should handle DISPLAY_NOTIFICATION_DRAWER', () => {
-    expect(uiReducer(initialState, { type: DISPLAY_NOTIFICATION_DRAWER })).toEqual({
-      ...initialState,
+    const newState = uiReducer(initialState, { type: DISPLAY_NOTIFICATION_DRAWER });
+    expect(newState.toJS()).toEqual({
+      ...initialState.toJS(),
       isNotificationDrawerVisible: true
     });
   });
 
   it('should handle HIDE_NOTIFICATION_DRAWER', () => {
-    expect(uiReducer(initialState, { type: HIDE_NOTIFICATION_DRAWER })).toEqual({
-      ...initialState,
+    const newState = uiReducer(initialState, { type: HIDE_NOTIFICATION_DRAWER });
+    expect(newState.toJS()).toEqual({
+      ...initialState.toJS(),
       isNotificationDrawerVisible: false
     });
   });
 
   it('should handle LOGIN_SUCCESS', () => {
-    expect(uiReducer(initialState, { type: LOGIN_SUCCESS })).toEqual({
-      ...initialState,
+    const newState = uiReducer(initialState, { type: LOGIN_SUCCESS });
+    expect(newState.toJS()).toEqual({
+      ...initialState.toJS(),
       isUserLoggedIn: true
     });
   });
 
   it('should handle LOGIN_FAILURE', () => {
-    expect(uiReducer(initialState, { type: LOGIN_FAILURE })).toEqual({
-      ...initialState,
+    const newState = uiReducer(initialState, { type: LOGIN_FAILURE });
+    expect(newState.toJS()).toEqual({
+      ...initialState.toJS(),
       isUserLoggedIn: false
     });
   });
 
   it('should handle LOGOUT', () => {
-    expect(uiReducer(initialState, { type: LOGOUT })).toEqual({
-      ...initialState,
+    const newState = uiReducer(initialState, { type: LOGOUT });
+    expect(newState.toJS()).toEqual({
+      ...initialState.toJS(),
       isUserLoggedIn: false
     });
   });
-
 });
