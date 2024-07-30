de164e24270099645a838b5d73883b87e7743495
Doreen Ikilai <dornkaizen@gmail.com>
2024-07-30 13:35:04 +0000

    feat: Add mock files, gitignore, and setup tests

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/schema/notifications.js
@@ -0,0 +1,35 @@
+import * as notificationsData from '../../../../notifications.json' assert { type: "json" };
+import { normalize, schema } from 'normalizr';
+
+const user = new schema.Entity('users');
+
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid' }
+);
+
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+const normalized = normalize(notificationsData.default, [notification]);
+
+// console.log('Normalized Data:', normalized);
+
+export { normalized, getAllNotificationsByUser };
+
+
+function getAllNotificationsByUser(userId){
+  const output = [];
+  const notifications = normalized.entities.notifications;
+  const messages = normalized.entities.messages;
+
+  for (const id in notifications) {
+    if (notifications[id].author === userId) {
+      const messageId = notifications[id].context;
+      output.push(messages[messageId]);
+    }
+  }
+  return output;
+}
+
