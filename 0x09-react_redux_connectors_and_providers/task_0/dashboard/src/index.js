79161e13996f78c944c8847e591c4ded6aeb181b
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-08 14:14:39 +0000

    chore: Connect App component to Redux store and provide store to App component

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import { createRoot } from "react-dom/client";
 import App from "./App/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import uiReducer, { initialState } from "./reducers/uiReducer";
 
 
+
+const store = createStore(uiReducer, Map(initialState));
 //react render in react 17
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
